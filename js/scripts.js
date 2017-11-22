97a4d2544a41a7913e5a95ef557687e99b54cdf0
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2017-11-22 22:16:47 +0100

    Add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt("Enter value for triangle base");
+var h = prompt("Enter value for triangle height");
+var triangleArea = a*h/2;
+
+console.log("Triangle field with base a = " + a +" and height h = " + h + " is equal to " + triangleArea);
+
+
