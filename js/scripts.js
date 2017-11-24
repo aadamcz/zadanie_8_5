f82c918055e8c56680a5c9abac92e000e0fc27f1
aadamcz <33779500+aadamcz@users.noreply.github.com>
2017-11-24 21:43:34 +0100

    Correct scripts.js formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var a = prompt("Enter value for triangle base");
 var h = prompt("Enter value for triangle height");
-var triangleArea = a*h/2;
+var triangleArea = a * h / 2;
 
 console.log("Triangle field with base a = " + a +" and height h = " + h + " is equal to " + triangleArea);
 
